bdc1534be36856512652ccdc3f0e613e8b3ac6af
hape <hapemartini@gmail.com>
2021-12-02 16:03:21 +0100

    add input fields

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
+import {Grid, Typography} from "@material-ui/core";
+import {FormProvider, useForm} from "react-hook-form";
+import FormInput from "./FormInput";
 
 const AddressForm = () => {
+    const methods = useForm();
+
     return (
-        <div>
-            AddressForm
-        </div>
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name='firstName' label="First Name"/>
+                        <FormInput required name='lastName' label="Last Name"/>
+                        <FormInput required name='address' label="Address"/>
+                        <FormInput required name='email' label="Email"/>
+                        <FormInput required name='city' label="City"/>
+                        <FormInput required name='zip' label="ZIP"/>
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
     );
 };
 
---
+++ src/components/CheckoutForm/FormInput.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Controller, useFormContext} from "react-hook-form";
+import {Grid, TextField} from "@material-ui/core";
+
+const FormInput = ({name, label, required}) => {
+    const {control} = useFormContext();
+
+    return (
+        <Grid item xs={12} sm={6}>
+            <Controller
+                as={TextField}
+                control={control}
+                fillwidth
+                name={name}
+                label={label}
+                required={required}
+            />
+        </Grid>
+    );
+};
+
+export default FormInput;
